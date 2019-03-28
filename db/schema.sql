fe29158106ab84428076c826899d588dde0ab256
Julie Mathews <juliem.forever@gmail.com>
2019-03-28 11:13:52 -0700

    schema code

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,10 @@
-   * Create the `burgers_db`.
-   * Switch to or use the `burgers_db`.
-   * Create a `burgers` table with these fields:
-     * **id**: an auto incrementing int that serves as the primary key.
-     * **burger_name**: a string.
-     * **devoured**: a boolean.
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+	id INTEGER AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(15) NOT NULL,
+    devoured BOOLEAN,
+    PRIMARY KEY (id)
+);
