2be391ce1e1e6421715795e623120f4edd5488c1
Julie Mathews <juliem.forever@gmail.com>
2019-03-28 22:29:50 -0700

    heroku JAWS

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var 
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
   password: "rootroot",
   database: "burgers_db"
-});
+  });
+};
 
 // Make connection
 connection.connect(function(err) {
